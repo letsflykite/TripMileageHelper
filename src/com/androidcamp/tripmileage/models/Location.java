fee6c99d90ec8d6220da94f756e2008e2b06984e
Joanne Sun <joannehsun@gmail.com>
2013-10-24 00:38:14 -0700

    Initial commit

---
+++ src/com/androidcamp/tripmileage/models/Location.java
@@ -0,0 +1,7 @@
+package com.androidcamp.tripmileage.models;
+
+public class Location {
+
+	String address;
+	//need to get address from phone GPS
+}
---
+++ src/com/androidcamp/tripmileage/models/TripRecord.java
@@ -0,0 +1,16 @@
+package com.androidcamp.tripmileage.models;
+
+import java.util.Date;
+
+public class TripRecord {
+   Location start;
+   Location end;
+   Date startTime;
+   Date endTime;
+   String imgUrl_start;
+   String imgUrl_end;
+   long mileage_start;
+   long mileage_end;
+   long mileage_ontrip;
+   String email_address_sent;
+}
